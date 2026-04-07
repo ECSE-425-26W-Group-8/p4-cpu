a72a972e85997946856c336db8a1695c021c7642
Wen-Chun <jennlinwc@gmail.com>
2026-04-06 23:16:03 -0400

    add control signals to tell ALU, memory, and wb what to do

--- processor/ID.vhd
+++ processor/ID.vhd
@@ -13,6 +13,15 @@ port(
 	inst_ID_EX_LNREG 	: out std_logic_vector(31 downto 0);
 	inst_MEM_ID_REGLN	: out std_logic_vector(31 downto 0);
 	data_WB_ID_LN		: out std_logic_vector(31 downto 0)
+    -- for control process
+	alu_src: out std_logic; -- tell rest of CPU to use imm or reg
+	alu_op : out std_logic_vector(3 downto 0); -- what ALU does
+	mem_read: out std_logic; -- meme access
+	mem_write: out std_logic; -- meme access
+	reg_write: out std_logic; -- write to reg
+	branch: out std_logic; -- control flow
+	jump: out std_logic; -- control flow
+	wb_sel: out std_logic_vector(1 downto 0); --what to write back
 ); 
 end ID;
 
@@ -144,6 +153,90 @@ begin
                 imm_val <= (others => '0');
         end case;
     end process;
+	-- control process
+	process(opcode, funct3, funct7)
+	begin
+    -- defaults 
+    alu_src <= '0';
+    alu_op <= "0000";
+    mem_read <= '0';
+    mem_write <= '0';
+    reg_write <= '0';
+    branch <= '0';
+    jump <= '0';
+    wb_sel <= "00";
+    case opcode is
+        -- R-TYPE (add, sub, mul, and, or) use rs1 rs2 for alu inputs
+        when "0110011" =>
+            reg_write <= '1'; -- write to rd
+            alu_src <= '0'; -- 2nd operandis from reg
+            wb_sel <= "00"; -- write back ALU result
+            case funct3 is --for add sub mul
+                when "000" => -- func3 is 0x00
+                    if funct7 = "0000000" then  --0x00
+                        alu_op <= "0000"; -- ADD rs1+rs2
+                    elsif funct7 = "0100000" then --0x20
+                        alu_op <= "0001"; -- SUB rs1-rs2
+                    elsif funct7 = "0000001" then --0x01
+                        alu_op <= "0010"; -- MUL rs1*rs2
+                    end if;
+                when "111" => -- func3 is 0x7
+                    alu_op <= "0011"; -- AND
+                when "110" => -- func3 is 0x6
+                    alu_op <= "0100"; -- OR
+                when others =>
+                    null;
+            end case;
+        -- I-TYPE (addi, andi, ori, xori) use rs1 and immVal
+        when "0010011" =>
+            reg_write <= '1'; --write result to rd
+            alu_src <= '1'; -- 2nd operand is from immval
+            wb_sel <= "00";
+            case funct3 is
+                when "000" => alu_op <= "0000"; -- ADDI rs1+imm
+                when "111" => alu_op <= "0011"; -- ANDI
+                when "110" => alu_op <= "0100"; -- ORI
+                when "100" => alu_op <= "0101"; -- XORI
+                when others => null;
+            end case;
+        -- LOAD (lw) addr : rs1 + imm
+        when "0000011" =>
+            reg_write <= '1';
+            alu_src <= '1'; -- use imm
+            mem_read <= '1'; -- enable mem read
+            wb_sel <= "01"; -- write back memory data
+            alu_op <= "0000"; -- use ADD for address calc
+        -- STORE (sw) addr: rs1 + imm
+        when "0100011" =>
+            alu_src <= '1';
+            mem_write <= '1'; -- enable mem write; nothing written to rd
+            alu_op <= "0000";-- ADD for add calc
+        -- BRANCH compare rs1 rs2
+        when "1100011" =>
+            branch <= '1';
+            alu_src <= '0'; -- compare 2 regs
+            alu_op <= "0001"; -- SUB for compare
+        -- JAL jump link to PC + imm
+        when "1101111" =>
+            jump <= '1';
+            reg_write <= '1';
+            wb_sel <= "10"; -- write PC+4
+        -- JALR jump and link reg to rs1 + imm
+        when "1100111" =>
+            jump <= '1';
+            reg_write <= '1';
+            alu_src <= '1'; compute target = rs1 + imm
+            wb_sel <= "10"; write pc + 4
+        -- LUI
+        when "0110111" =>
+            reg_write <= '1';
+            alu_src <= '1';
+            wb_sel <= "00";
+        when others =>
+            null;
+    end case;
+end process;
+
     -- Outputs to EX stage
     addr_ID_EX_LNREG <= addr_IF_ID_REGLN;
     op1_ID_EX_LNREG <= op1_val;
